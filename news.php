5a77a64604f1d8cbb69718d3a08c408993e41717
blacksk81 <blacksk81@gmail.com>
2018-10-08 16:31:11 -0500

    Ubicaciones 003

--- news.php
+++ news.php
@@ -1,7 +1,12 @@
 <?php get_header( 'page' ) //template name: news?>
 
+<!-- ESTA SERIA UNA PAGINA EN BLANCO -->
+
+<!-- AQUI ADENTRO TODO SU CONTENIDO -->
 
 <h1><?php the_field( 'news' ); ?></h1>
 <p><?php the_field( 'news_text' ); ?></p>
+
+<!-- AQUI ADENTRO TODO SU CONTENIDO -->
 
 <?php get_footer(); ?>
